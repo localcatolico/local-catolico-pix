29cd1aceb73028d245d964b3cfb2702987240aa3
Rafael Mateus <rafaelbmateus.dev@gmail.com>
2023-06-27 21:35:07 -0300

    chore: add copy icon

--- assets/js/churchs.js
+++ assets/js/churchs.js
@@ -17,7 +17,15 @@ function churchs(data) {
         <div id="flush-collapse` + church.id + `" class="accordion-collapse collapse" aria-labelledby="` + church.id + `" data-bs-parent="#churchs">
           <div class="accordion-body">
             <p>Faça sua oferta para <b>` + church.name + `</b></p>
-            <p>A chave PIX: <b>` + church.pix + `</b> <button class="btn btn-sm btn-secondary" onclick="copyToClipboard('` + church.pix + `')">Copiar</button></p>
+            <p>
+              A chave PIX: <b>` + church.pix + `</b>
+              <button class="btn btn-sm btn-secondary" onclick="copyToClipboard('` + church.pix + `')">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
+                  <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z"/>
+                </svg>
+                Copiar
+              </button>
+            </p>
             <img src="assets/img/` + church.id + `.png" width="250" height="250"/><br><br>
             <a class="btn btn-danger" href="` + church.instagram + `">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
